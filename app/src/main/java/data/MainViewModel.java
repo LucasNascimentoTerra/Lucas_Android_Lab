9e3092774a1a4dc00ed2339fbb5b500f561a4a96
Lucas Nascimento Terra <nasc0014@algonquinlive.com>
2023-06-02 11:35:52 -0400

    Finished week 2

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -5,5 +5,13 @@ import androidx.lifecycle.ViewModel;
 public class MainViewModel extends ViewModel {
 
     public MutableLiveData<String> editString = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
 
+    public MutableLiveData<Boolean> getIsSelected() {
+        return isSelected;
+    }
+
+    public void setIsSelected(boolean value) {
+        isSelected.setValue(value);
+    }
 }
--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,5 +1,9 @@
 package ui;
 
+import static android.app.ProgressDialog.show;
+import static android.opengl.ETC1.getHeight;
+import static android.opengl.ETC1.getWidth;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.nasc0014.databinding.ActivityMainBinding;
 import data.MainViewModel;
@@ -28,16 +33,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(variableBinding.getRoot());
 
 
-        variableBinding.textview.setText(model.editString);
 
-        variableBinding.mybutton.setOnClickListener(click  ->
-        {
-                model.editString.postValue(variableBinding.myedittext.getText().toString());
-            model.editString.observe(this, s ->  {
-                variableBinding.textview.setText("Your edit text has: " + model.editString);
-            });
 
-    });
+        variableBinding.textview.setText(model.editString.getValue());
+
+
+
+
+        variableBinding.mybutton.setOnClickListener(click  -> {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+        });
+
+        model.editString.observe(this, s -> {
+            variableBinding.textview.setText("Your edit text has: " + s);
+        });
+
+
+
+        variableBinding.myimagebutton.setOnClickListener(view ->{
+            String message = "width = " + variableBinding.myimagebutton.getWidth() + " height = " + variableBinding.myimagebutton.getHeight();
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        });
+
+
+
+        //checkbox
+        variableBinding.checkbox.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.getIsSelected().postValue(isChecked);
+        });
+
+        // radio button
+        variableBinding.radiobutton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.getIsSelected().postValue(isChecked);
+        });
+
+        // switch button
+        variableBinding.switchbutton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.getIsSelected().postValue(isChecked);
+        });
+
+
+
+        model.isSelected.observe(this, isSelected -> {
+            variableBinding.checkbox.setChecked(isSelected);
+            variableBinding.radiobutton.setChecked(isSelected);
+            variableBinding.switchbutton.setChecked(isSelected);
+                });
+
 
     }
 }
